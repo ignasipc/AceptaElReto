fc07e723c711f553a2bd141c1d17e0f5842ad134
ignasipc <ignasipc22@gmail.com>
2026-01-22 12:27:34 +0100

    Solved Wrong Answers problem

--- V1_P101.java
+++ V1_P101.java
@@ -8,7 +8,7 @@ public class V1_P101 {
         if (n == 0)
             return false;
         else {
-            boolean resultadoIndicado = false;
+            boolean esDiabolico = true;
             int sqrn = n * n;
             PriorityQueue <Integer> colaPrioridades = new PriorityQueue <> (sqrn);
             int [][] matriz = new int[n][n];
@@ -22,97 +22,90 @@ public class V1_P101 {
 
             // Obtenemos la constante mágica
             int CM = 0;
+            for (int i = 0; i < n; i++) {
+                CM += matriz[0][i];
+            }
+
             int cmFilas, cmColumnas, cmDiagonalPrin = 0, cmDiagonalSec = 0;
+
+            // Comprobamos las filas y columnas
             for (int i = 0; i < n; i++) {
                 cmFilas = 0;
                 cmColumnas = 0;
                 for (int j = 0; j < n; j++) {
-                    if (i == 0){
-                        CM+=matriz[0][j];
-                    }
-
-                    if (i==j){
-                        cmDiagonalPrin += matriz[i][j];
-                    }
-
-                    if (i+j == n-1){
-                        cmDiagonalSec += matriz[i][j];
-                    }
-
                     cmFilas += matriz[i][j];
                     cmColumnas += matriz[j][i];
                 }
-                if ((CM != cmFilas || CM != cmColumnas) && !resultadoIndicado){
-                    System.out.println("NO");
-                    resultadoIndicado = true;
+                if (CM != cmFilas || CM != cmColumnas){
+                    esDiabolico = false;
+                    break;
                 }
             }
-            if ((CM != cmDiagonalPrin || CM != cmDiagonalSec) && !resultadoIndicado){
-                resultadoIndicado = true;
+
+            // Seguimos comprobando que es mágico
+            if (esDiabolico){
+                for (int i = 0; i < n; i++) {
+                    cmDiagonalPrin += matriz[i][i];
+                    cmDiagonalSec += matriz[i][n - 1 - i];
+                }
+                if (CM != cmDiagonalPrin || CM != cmDiagonalSec){
+                    esDiabolico = false;
+                }
+            }
+            
+            if (!esDiabolico){
                 System.out.println("NO");
+                return true;
             }
 
             // Comprobación 1
             for (int i = 1; i <= sqrn; i++) {
                 if (i != colaPrioridades.poll()){
                     System.out.println("DIABOLICO");
-                    resultadoIndicado = true;
-                    break;
+                    return true;
                 }
             }
 
             // Comprobación 2
-            if (!resultadoIndicado){
-                int CM2 = 4*CM /n;
-                int sumaEsquinas = matriz[0][0] + matriz[0][n-1] + matriz[n-1][0] + matriz[n-1][n-1];
+            int CM2 = (4*CM) / n;
+            int sumaEsquinas = matriz[0][0] + matriz[0][n-1] + matriz[n-1][0] + matriz[n-1][n-1];
 
-                if (CM2 != sumaEsquinas){
-                    System.out.println("DIABOLICO");
-                    resultadoIndicado = true;
-                }
-                if (!resultadoIndicado){
-                    if (n%2 != 0){ // impar
-
-                        int mitad = n/2;
-                        int sumaMitades = matriz[0][mitad] + matriz[mitad][0] + matriz[mitad][n-1] + matriz[n-1][mitad];
-
-                        if (CM2 != sumaMitades){
-                            System.out.println("DIABOLICO");
-                            resultadoIndicado = true;
-                        }
-
-                        if(!resultadoIndicado){
-                            if (matriz[mitad][mitad]*4 != CM2){
-                                System.out.println("DIABOLICO");
-                                resultadoIndicado = true;
-                            }
-                        }
-
-                    }else{  //par
-
-                        int mitad = n/2;
-                        int sumaMitades = matriz[0][mitad] + matriz[mitad][0] + matriz[mitad][n-1] + matriz[n-1][mitad]+
-                                matriz[0][mitad-1] + matriz[mitad-1][0] + matriz[mitad-1][n-1] + matriz[n-1][mitad-1];
-
-                        if (2*CM2 != sumaMitades){
-                            System.out.println("DIABOLICO");
-                            resultadoIndicado = true;
-                        }
-
-                        if(!resultadoIndicado){
-                            int centro = matriz[mitad][mitad] + matriz[mitad-1][mitad] + matriz[mitad][mitad-1] + matriz[mitad-1][mitad-1];
-                            if (centro != CM2){
-                                System.out.println("DIABOLICO");
-                                resultadoIndicado = true;
-                            }
-                        }
-
-                    }
-                    if (!resultadoIndicado){
-                        System.out.println("ESOTERICO");
-                    }
-                }
+            if (CM2 != sumaEsquinas){
+                System.out.println("DIABOLICO");
+                return true;
             }
+
+            // División entre par e impar
+            int mitad = n / 2;
+            if ((n % 2) != 0){ // impar
+                int sumaMitades = matriz[0][mitad] + matriz[mitad][0] + matriz[mitad][n-1] + matriz[n-1][mitad];
+
+                if (CM2 != sumaMitades){
+                    System.out.println("DIABOLICO");
+                    return true;
+                }
+
+                if ((matriz[mitad][mitad] * 4) != CM2){
+                    System.out.println("DIABOLICO");
+                    return true;
+                }
+            }else{  //par
+                int sumaMitades = matriz[0][mitad] + matriz[mitad][0] + matriz[mitad][n-1] + matriz[n-1][mitad]+
+                        matriz[0][mitad-1] + matriz[mitad-1][0] + matriz[mitad-1][n-1] + matriz[n-1][mitad-1];
+
+                if (2*CM2 != sumaMitades){
+                    System.out.println("DIABOLICO");
+                    return true;
+                }
+
+                int centro = matriz[mitad][mitad] + matriz[mitad-1][mitad] + matriz[mitad][mitad-1] + matriz[mitad-1][mitad-1];
+                if (centro != CM2){
+                    System.out.println("DIABOLICO");
+                    return true;
+                }
+
+            }
+            System.out.println("ESOTERICO");
             return true;
         }
     }
