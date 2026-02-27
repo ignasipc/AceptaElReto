b27d9dafc2509407f59596266afc8a057e7c1b0c
ignasipc <ignasipc22@gmail.com>
2026-02-27 09:07:22 +0100

    Mejorada eficiencia de la P679

--- V1_P679.java
+++ V1_P679.java
@@ -1,17 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class V1_P679 {
-    static java.util.Scanner in;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
 
-    static Deque<String> pila = new ArrayDeque<>();
-    static Deque<String> pilaBorrado = new ArrayDeque<>();
+    public static void casoDePrueba() throws IOException {
+        Deque<String> pila = new ArrayDeque<>();
+        Deque<String> pilaBorrado = new ArrayDeque<>();
 
-    public static void casoDePrueba(){
-        pila.clear();
-        pilaBorrado.clear();
-
-        String palabra = in.next();
+        st = new StringTokenizer(br.readLine());
+        String palabra = st.nextToken();
         while(!palabra.equals(".")){
             if (palabra.equals("<")) {  // Pulsa Ctrl + Z
                 if (!pila.isEmpty()){
@@ -29,25 +32,19 @@ public class V1_P679 {
                 pila.push(palabra);
                 pilaBorrado.clear();
             }
-            palabra = in.next();
+            palabra = st.nextToken();
         }
 
-        if (!pila.isEmpty()) {
-            StringBuilder frase = new StringBuilder(pila.pop());
-            while (!pila.isEmpty()) {
-                frase.insert(0, pila.pop() + " ");
-            }
-            System.out.println(frase);
-        } else {
-            System.out.println();
+        StringBuilder frase = new StringBuilder();
+        for (java.util.Iterator<String> it = pila.descendingIterator(); it.hasNext();) {
+            frase.append(it.next());
+            if (it.hasNext()) frase.append(" ");
         }
+        System.out.println(frase);
     }
 
-    public static void main(String[] args) {
-        in = new java.util.Scanner(System.in);
-
-        int n = in.nextInt();
-        for (int i = 0; i < n; i++)
-            casoDePrueba();
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) casoDePrueba();
     }
 }
